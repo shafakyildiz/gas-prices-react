ea1dc8480b79155d895316c4eea3dfe707578e04
Safak Yildiz <shafakyildiz@gmail.com>
2022-06-11 21:47:45 +0300

    fetched the data and printed on the main screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  var apiUrl = "https://api.collectapi.com/gasPrice/turkeyLpg?city=ankara";
-  var apiKey = "1FkC4bAfQ1jh0Tb9Hxx9ET:2W9TQ6qj8A6SBw4UOpJQzv";
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    var apiUrl = "https://api.collectapi.com/gasPrice/turkeyLpg?city=ankara";
+    var apiKey = "1FkC4bAfQ1jh0Tb9Hxx9ET:2W9TQ6qj8A6SBw4UOpJQzv";
     const fetchData = () => {
       axios.interceptors.request.use(
         (config) => {
@@ -30,27 +29,26 @@ function App() {
         key: apiKey,
       };
 
-      axios
-        .get(apiUrl, bodyParameters, config)
-        .then((response) => console.log(response));
+      axios.get(apiUrl, bodyParameters, config).then((response) => {
+        console.log(response);
+        setData(response.data.result);
+      });
     };
     fetchData();
   }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {data.map((item) => {
+            console.log(item.lpg);
+            return (
+              <li>
+                {item.marka} - {item.lpg}
+              </li>
+            );
+          })}
+        </ul>
       </header>
     </div>
   );
